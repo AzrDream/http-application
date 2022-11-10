038e3a0335059b8de2e90239f4be498765f70138
unknown <jiakaiwen@ESG.360ES.CN>
2022-11-10 18:50:20 +0800

    cookie

---
+++ cookie/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    // 目的是在访问url后将同目录下的index.html展示出来
+    // 拼接路径
+    const url = path.join(__dirname, 'index.html');
+    // 同步读取文件
+    const html = fs.readFileSync(url, 'utf8');
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Set-Cookie': ['id=123', 'abc=456']
+    });
+    // 上面设置的响应头，以html文件返回
+    res.end(html);
+}).listen(8887);
